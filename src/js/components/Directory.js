943052d30eb048749ebebeb7720be53794bb6b14
Dave Woods <dwoodscs@gmail.com>
2017-04-04 17:00:06 +0100

    removed console log from Directory component

--- src/js/components/Directory.js
+++ src/js/components/Directory.js
@@ -3,7 +3,6 @@ import React from 'react'
 export default class Directory extends React.Component {
 	render() {
 		const links = this.props.children;
-		console.log(links);
 		return (
 			<ul>
 				{ React.Children.map(links, (child, i) => <li key={i}>{child}</li>) }
