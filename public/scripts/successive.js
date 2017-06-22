866e31e46a5259adc806b9973a5d3ba467c51fbf
Dave Woods <dwoodscs@gmail.com>
2017-06-22 17:25:38 +0100

    Set up project for multiple react pages

---
+++ public/scripts/successive.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Root = (props) => {
+  return <h1>Root</h1>
+}
+
+ReactDOM.render(<Root />, document.getElementById('successive-app'))
