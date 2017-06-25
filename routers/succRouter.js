82b53e91c52bbbfcfaf4afa38970f10c9eb0be3d
Dave Woods <dwoodscs@gmail.com>
2017-06-25 05:19:46 +0100

    Added v simple todo list

--- routers/succRouter.js
+++ routers/succRouter.js
@@ -6,4 +6,40 @@ router.get('/', (req, res) => {
   })
 })
 
+router.get('/todo', (req, res) => {
+  res.render('successive/todo', {
+    title: 'To Do',
+    todos: [
+      {
+        desc: 'Gradient styles',
+        completed: false
+      },
+      {
+        desc: 'Add new tasks',
+        completed: false
+      },
+      {
+        desc: 'Daily labels',
+        completed: false
+      },
+      {
+        desc: 'One completion a day',
+        completed: false
+      },
+      {
+        desc: 'Check if day is missed',
+        completed: false
+      },
+      {
+        desc: 'Grid-like styling',
+        completed: false
+      },
+      {
+        desc: 'Updateable todos',
+        completed: false
+      }
+    ]
+  })
+})
+
 module.exports = router
